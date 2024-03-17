c26a258f0cf77515e7a60a4cab31cce137bc393c
Naimul Islam Siam <naimul.siam@welldev.io>
2024-03-17 14:51:11 +0600

    feat: create a new blog post

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
-  get "posts/:id", to: "blog_posts#show", as: :blog_post
+  get 'posts/new', to: 'blog_posts#new', as: :new_blog_post
+  get 'posts/:id', to: 'blog_posts#show', as: :blog_post
+
+  post '/post', to: 'blog_posts#create', as: :blog_posts
 
   # Defines the root path route ("/")
-  root "blog_posts#index"
+  root 'blog_posts#index'
 end
